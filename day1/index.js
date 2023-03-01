3d6ec84024fdfc8bd5deab1e6a47c901d4313f56
Shishir Lamichhane <103579434+dshishir13@users.noreply.github.com>
2023-03-01 12:55:51 +0545

    Add files via upload

---
+++ day1/index.js
@@ -0,0 +1,12 @@
+const array = [11,3,7,8,90,2,33];
+let i,smallest,largest;
+largest=smallest=array[0];
+for(i=0;i<=array.length;i++){
+    if(array[i]>largest)
+    largest=array[i];
+    if(array[i]<smallest)
+    smallest=array[i];
+}
+
+console.log("largest="+largest);
+console.log("smallest="+smallest);
